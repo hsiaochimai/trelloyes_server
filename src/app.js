c5a3f93e8d873b4aa7ab6109c66870634447470e
Jennifer Hsiao <jchmai21@gmail.com>
2019-04-30 11:44:25 -0700

    GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cardRouter = require('./card/card-router')
+const listRouter = require('./list/list-router')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
 const app = express()
-
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
+  
 
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
+app.use(express.json());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
 
-app.get('/',(req,res)=>{
-  res.send('Hello, world!')
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
 })
+
+app.use(cardRouter)
+app.use(listRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
